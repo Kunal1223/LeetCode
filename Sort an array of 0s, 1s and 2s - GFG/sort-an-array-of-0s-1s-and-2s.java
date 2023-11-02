0336fcf70eee1e969306099bf96452301a2ac42c
Kunal Kishor <kunalbth2002@gmail.com>
2023-11-02 23:05:13 +0530

    Update sort-an-array-of-0s-1s-and-2s.java

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
@@ -1,45 +1,31 @@
-//{ Driver Code Starts
-//Initial template for Java
-
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
 //User function template for Java
 
 class Solution
 {
-    public static void sort012(int a[], int n)
+    public static void sort012(int arr[], int n)
     {
-        Arrays.sort(a);
-    }
-}
+        int low = 0, mid = 0, high = n - 1; // 3 pointers
 
-//{ Driver Code Starts.
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                // swapping arr[low] and arr[mid]
+                int temp = arr[low];
+                arr[low] =  arr[mid];
+                arr[mid] = temp;
 
-class GFG {
-    
-    public static void main (String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
-        while(t-->0){
-            int n = Integer.parseInt(br.readLine().trim());
-            int arr[] = new int[n];
-            String inputLine[] = br.readLine().trim().split(" ");
-            for(int i=0; i<n; i++){
-                arr[i] = Integer.parseInt(inputLine[i]);
+                low++;
+                mid++;
+
+            } else if (arr[mid] == 1) {
+                mid++;
+
+            } else {
+                // swapping arr[mid] and arr[high]
+                int temp = arr[mid];
+                arr[mid]=arr[high];
+                arr[high] = temp;
+                high--;
             }
-            Solution ob=new Solution();
-            ob.sort012(arr, n);
-            StringBuffer str = new StringBuffer();
-            for(int i=0; i<n; i++){
-                str.append(arr[i]+" ");
-            }
-            System.out.println(str);
         }
     }
 }
-
-
-// } Driver Code Ends
