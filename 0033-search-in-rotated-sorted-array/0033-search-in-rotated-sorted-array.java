c51fb9e004360681714fdf957a91a8e4766881a0
Kunal Kishor <kunalbth2002@gmail.com>
2023-11-30 17:00:29 +0530

    Time: 0 ms (100.00%), Space: 41 MB (42.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,66 +1,34 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-        int peeknum = Findpeek(nums);
-        
-        if (peeknum == -1){
-            return binarySearch(nums , target , 0 , nums.length -1 );
+        int len = nums.length;
+
+        int s = 0;
+        int e = len - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[s] <= nums[mid]) {
+                // Left half is sorted
+                if (nums[s] <= target && target < nums[mid]) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            } else {
+                // Right half is sorted
+                if (nums[mid] < target && target <= nums[e]) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
         }
-        
-        if(nums[peeknum] == target){
-            return peeknum;
-        }
-        
-        if(target == nums[0]){
-            return 0;
-        }
-        
-        if(target > nums[0]) {
-            return binarySearch(nums , target , 0 , peeknum-1 );
-        }else{
-             return binarySearch(nums , target , peeknum+1 , nums.length -1 );
-        }
-      
+
+        return -1;
     }
-        
-         int Findpeek(int[] arr){            
-            int start = 0;
-            int end = arr.length - 1 ; 
-            
-            while(start <= end){
-                
-                int mid = start + (end - start)/2;
-                
-                if(mid < end && arr[mid] > arr[mid+1]) {
-                    return mid ; 
-                }
-                if(mid> start && arr[mid] < arr[mid-1]) {
-                    return mid-1;
-                }
-                if (arr[mid] < arr[start]){
-                    end = mid -1;
-                }else{
-                    start = mid +1;
-                }
-            }
-              return -1;
-        }  
-        
-        
-         int binarySearch(int arr[] , int target, int start , int end){        
-            while(start <= end ){
-                int mid = start + (end - start)/2;
-                
-                if(arr[mid] == target){
-                    return mid ;
-                }
-                else if ( arr[mid] < target){
-                    start = mid +1 ;
-                }
-                else{
-                    end = mid -1 ;
-                }
-            }
-            return -1 ;
-        }
 }
