ab677abb06a05767073b5674f6d0a849baee5ca9
Kunal Kishor <kunalbth2002@gmail.com>
2024-02-17 01:58:34 +0530

    Time: 20 ms (27.26%), Space: 55.3 MB (85.87%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,18 +1,12 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        
-        for (int num : nums) {
-            if (set.contains(num)) {
-                return true; // Found a duplicate
-            } else {
-                set.add(num); // Add the number to the set
-            }
+        Arrays.sort(nums); 
+        for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i+1]) {
+                    return true;
+                }
         }
-        
-        return false; // No duplicates found
+        return false;
     }
 }
+
