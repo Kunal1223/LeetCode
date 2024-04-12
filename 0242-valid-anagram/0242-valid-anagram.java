ec4c7e3052abc1593018ff7784fb813a75995497
Kunal Kishor <kunalbth2002@gmail.com>
2024-04-12 11:25:27 +0530

    Time: 2 ms (98.33%), Space: 43.2 MB (67.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,19 +3,19 @@ class Solution {
         if (s.length() != t.length())
             return false;
         
-        int[] count = new int[26]; // Assuming only lowercase English alphabets
+        int[] count = new int[26]; 
         
-        // Increment counts for characters in string s
+        
         for (char ch : s.toCharArray()) {
             count[ch - 'a']++;
         }
         
-        // Decrement counts for characters in string t
+        
         for (char ch : t.toCharArray()) {
             count[ch - 'a']--;
         }
         
-        // If all counts are zero, the strings are anagrams
+        
         for (int i = 0; i < 26; i++) {
             if (count[i] != 0)
                 return false;
