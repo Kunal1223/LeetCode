c24c5733dbd9580871d78cbff7b52f6d0d3bcf92
Kunal Kishor <kunalbth2002@gmail.com>
2024-04-23 01:23:04 +0530

    Time: 87 ms (5.38%), Space: 67.9 MB (63.87%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -1,46 +1,41 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        ListNode cur = head;
-        List<ListNode> list = new ArrayList<>();
-        while (cur != null) {
-            list.add(cur);
-            cur = cur.next;
-        }
-        int max = 0;
-        int[] arr = new int[list.size()];
-        //finding max from the end to include values into result
-        for (int i = list.size() - 1; i >= 0; i--) {
-            if (max <= list.get(i).val) {
-                arr[i] = 1;
-                max = list.get(i).val;
+        Stack<Integer> st = new Stack<>();
+
+        ListNode curr = head;
+        ListNode ans = null;
+
+        while(curr != null){
+            
+            if(st.isEmpty()){
+                st.push(curr.val);
             }
-        }
-        boolean headSet = false;
-        cur = head;
-        //adding nodes to the result
-        for (int i = 0; i < list.size(); i++) {
-            if (arr[i] == 1) {
-                if (!headSet) {
-                    headSet = true;
-                    head = list.get(i);
-                    cur = head;
-                } else {
-                    cur.next = list.get(i);
-                    cur = cur.next;
+
+            else{
+
+                if(st.peek() > curr.val){
+                    st.push(curr.val);
+                }
+
+                else{
+                    while(!st.isEmpty() && st.peek() < curr.val){
+                        st.pop();
+                    }
+                    st.push(curr.val);
                 }
             }
+
+            curr = curr.next;
         }
-        cur.next = null;
-        return head;
+
+        while(!st.isEmpty()){
+            ListNode list = new ListNode(st.pop());
+            list.next = ans;
+            ans = list;
+        }
+
+        return ans;
+
+        
     }
 }
