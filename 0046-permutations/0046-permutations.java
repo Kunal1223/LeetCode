ddc09e9537d8fda9e925e76e6d399066d030b42b
Kunal Kishor <kunalbth2002@gmail.com>
2024-04-24 18:28:36 +0530

    Time: 0 ms (100.00%), Space: 44 MB (95.73%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,32 +1,72 @@
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+        
+//         List<List<Integer>> result = new ArrayList<>();
+//         Backtracking(result , new ArrayList<>() , nums);
+//         return result;
+        
+//     }
+    
+    
+//     public void Backtracking(List<List<Integer>> result ,List<Integer> temp , int nums[]){
+
+//         if(temp.size() == nums.length){
+//             result.add(new ArrayList<>(temp));
+//             return;
+//         }
+        
+//         for(int number : nums){
+            
+//             if(temp.contains(number)){
+//                 continue;
+//             }
+            
+//             temp.add(number);
+//              Backtracking(result , temp , nums);
+            
+//             temp.remove(temp.size()-1);
+            
+//         }
+//     }
+    
+// }
+
+// ############# above method is also working fine and this one is also working $$$$$$$$$$$$$$$$$$
+
 class Solution {
+
+    public void swapNumber(int i , int j , int nums[]){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void BackTrackByMe(int index , List<List<Integer>> result , int nums[]){
+
+        if(index >= nums.length){
+            List<Integer> list = new ArrayList<>();
+            for(int i : nums){
+                list.add(i);
+            }
+            result.add(list);
+        }
+
+        for(int j = index ; j < nums.length ; j++){
+            swapNumber(index , j , nums);
+            BackTrackByMe(index+1 , result , nums);
+            swapNumber(j , index , nums);
+        }
+
+    }
+
+
     public List<List<Integer>> permute(int[] nums) {
         
         List<List<Integer>> result = new ArrayList<>();
-        Backtracking(result , new ArrayList<>() , nums);
-        return result;
-        
-    }
-    
-    
-    public void Backtracking(List<List<Integer>> result ,List<Integer> temp , int nums[]){
+        int index = 0;
 
-        if(temp.size() == nums.length){
-            result.add(new ArrayList<>(temp));
-            return;
-        }
-        
-        for(int number : nums){
-            
-            if(temp.contains(number)){
-                continue;
-            }
-            
-            temp.add(number);
-             Backtracking(result , temp , nums);
-            
-            temp.remove(temp.size()-1);
-            
-        }
+        BackTrackByMe(index , result , nums);
+
+        return result;
     }
-    
 }
