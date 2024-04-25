a8197435f29f0d12eed568eea9fb55250bc9b34f
Kunal Kishor <kunalbth2002@gmail.com>
2024-04-26 00:45:42 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (29.24%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,46 +1,20 @@
 public class Solution {
-
-    public boolean checkCircle(ListNode head) {
-        if (head == null || head.next == null) return false;
-        
-        ListNode slow1 = head;
-        ListNode fast1 = head.next;
-
-        while (fast1 != null && fast1.next != null) {
-            if (slow1 == fast1) {
-                return true;
-            }
-            slow1 = slow1.next;
-            fast1 = fast1.next.next;
-        }
-
-        return false;
-    }
-
     public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null) return null;
-
-        if (!checkCircle(head)) return null;
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null ) {
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if(fast == slow){
-                break;
+        ListNode slow = head , fast = head;
+        
+        while(fast!=null&&fast.next!=null){
+            
+            fast=fast.next.next;
+            slow=slow.next;
+            
+            if(fast==slow){
+                while(head!=slow){
+                    head=head.next;
+                    slow=slow.next;
+                }
+                return slow;
             }
         }
-
-        slow = head;
-
-        while (slow != fast) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-
-        return fast;
+        return null;
     }
 }
