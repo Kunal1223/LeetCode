fd78b1cbfe94678066df9a11fb6f4ee4159fb779
Kunal Kishor <kunalbth2002@gmail.com>
2024-04-26 11:22:40 +0530

    Time: 5 ms (89.90%), Space: 83.5 MB (27.48%) - LeetHub

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode move = dummy;
+        ListNode curr = head.next;
+        int sum = 0;
+
+        while(curr != null){
+
+            if(curr.val != 0){
+                sum += curr.val;
+            }else{
+                move.next = new ListNode(sum);
+                move = move.next;
+                sum = 0;
+            }
+
+            curr = curr.next;
+        }
+    
+        return dummy.next;
+    }
+}
