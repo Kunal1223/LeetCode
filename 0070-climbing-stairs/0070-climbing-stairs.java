f5cd3bb464e11e6d9e8aa90043d407a75e6bdc7d
Kunal Kishor <kunalbth2002@gmail.com>
2024-04-27 18:08:02 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (96.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,52 @@
-class Solution {
-    
-    int solve(Map<Integer , Integer> mp , int n){
+// ############### Solve By Using HashMap ######################################
 
-        if(n == 0 || n == 1){
-            return 1;
-        }
-        
-        if(!mp.containsKey(n)){
-            mp.put(n , solve(mp , n-1) + solve(mp,n-2));
-        }
-        
-        return mp.get(n);
+// class Solution {
     
+//     int solve(Map<Integer , Integer> mp , int n){
+
+//         if(n == 0 || n == 1){
+//             return 1;
+//         }
+        
+//         if(!mp.containsKey(n)){
+//             mp.put(n , solve(mp , n-1) + solve(mp,n-2));
+//         }
+        
+//         return mp.get(n);
+    
+//     }
+    
+    
+//     public int climbStairs(int n) {
+        
+//         Map<Integer , Integer> mp = new HashMap<>();
+        
+//         return solve(mp , n);
+        
+//     }
+// }
+
+
+// ################  Solve By Using Dynamic Programing #################
+
+class Solution {
+
+    public int SolveByDynamic(int dp[] , int n){
+        if(n == 1 || n == 0) return 1;
+
+        if(dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = SolveByDynamic(dp , n-1) + SolveByDynamic(dp , n-2);
     }
-    
-    
+
     public int climbStairs(int n) {
-        
-        Map<Integer , Integer> mp = new HashMap<>();
-        
-        return solve(mp , n);
+        if(n == 1 || n == 0 ) return 1;
+
+        int dp[] = new int [n+1];
+        Arrays.fill(dp , -1);
+
+        return SolveByDynamic(dp , n);
         
     }
 }
