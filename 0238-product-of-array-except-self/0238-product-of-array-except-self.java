133bd509258c4b006251646b673121bd6be4ff0b
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-02 23:11:07 +0530

    Time: 2 ms (80.09%), Space: 56.1 MB (6.60%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,53 +1,78 @@
+// ################################### division approch $$$$$$$$$$$$$$$$$$$$$
+
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+        // int multi = 1, k = 0;
+        // int ans[] = new int[nums.length];
+
+        // for (int i = 0; i < nums.length; i++) {
+        // multi *= nums[i];
+        // }
+
+        // if (multi == 0) {
+        // int count = 0;
+        // multi = 1;
+
+        // for (int i = 0; i < nums.length; i++) {
+
+        // if (nums[i] == 0) {
+        // count++;
+        // continue;
+        // } else {
+        // multi *= nums[i];
+        // }
+        // }
+
+        // if (count > 1) {
+        // k = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        // ans[k++] = 0;
+        // }
+        // } else {
+        // for (int i = 0; i < nums.length; i++) {
+
+        // if (nums[i] == 0) {
+        // ans[i] = multi;
+        // } else {
+        // ans[i] = 0;
+        // }
+        // }
+        // }
+        // }
+
+        // else {
+        // for (int i = 0; i < nums.length; i++) {
+
+        // int val = multi / nums[i];
+        // ans[k++] = val;
+
+        // }
+        // }
+
+//         return ans;
+//     }
+// }
+
+// ############################## with 0(n) space complexity ##############
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int pre[] = new int[n];
+        int suff[] = new int[n];
+        pre[0] = 1;
+        suff[n - 1] = 1;
 
-        int multi = 1, k = 0;
-        int ans[] = new int[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            multi *= nums[i];
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            suff[i] = suff[i + 1] * nums[i + 1];
         }
 
-        if (multi == 0) {
-            int count = 0;
-            multi = 1;
-
-            for (int i = 0; i < nums.length; i++) {
-
-                if (nums[i] == 0) {
-                    count++;
-                    continue;
-                } else {
-                    multi *= nums[i];
-                }
-            }
-
-            if (count > 1) {
-                k = 0;
-                for (int i = 0; i < nums.length; i++) {
-                    ans[k++] = 0;
-                }
-            } else {
-                for (int i = 0; i < nums.length; i++) {
-
-                    if (nums[i] == 0) {
-                        ans[i] = multi;
-                    } else {
-                        ans[i] = 0;
-                    }
-                }
-            }
+        int ans[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = pre[i] * suff[i];
         }
-
-        else {
-            for (int i = 0; i < nums.length; i++) {
-
-                int val = multi / nums[i];
-                ans[k++] = val;
-
-            }
-        }
-
         return ans;
     }
 }
