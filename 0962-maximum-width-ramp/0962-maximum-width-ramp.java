1b6f5a186bc8ae069e031fcbf05293f2eb3d678e
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-06 00:35:37 +0530

    Time: 16 ms (36.77%), Space: 54.1 MB (28.69%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.java
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -1,28 +1,58 @@
+// class Solution {
+//     public int maxWidthRamp(int[] nums) {
+//         int n = nums.length;
+//         if (n == 0) return 0;
+        
+//         int[] minval = new int[n];
+//         minval[0] = nums[0];
+        
+//         for(int i= 1; i < n ; i++){
+//             minval[i] = Math.min(minval[i-1] , nums[i]);
+//         }
+
+//         int i = n-1, j = n-1 ;
+//         int dist = Integer.MIN_VALUE;
+
+//         while(i>=0 && j >= 0){
+
+//             if(nums[i] >= minval[j]){
+//                 dist = Math.max(dist , (i-j));
+//                 j--;
+//             }else{
+//                 i--;
+//             }
+//         }
+
+//         return dist;
+//     }
+// }
+
+
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$ wiht stack $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 class Solution {
     public int maxWidthRamp(int[] nums) {
         int n = nums.length;
         if (n == 0) return 0;
         
-        int[] minval = new int[n];
-        minval[0] = nums[0];
-        
-        for(int i= 1; i < n ; i++){
-            minval[i] = Math.min(minval[i-1] , nums[i]);
-        }
-
-        int i = n-1, j = n-1 ;
-        int dist = Integer.MIN_VALUE;
-
-        while(i>=0 && j >= 0){
-
-            if(nums[i] >= minval[j]){
-                dist = Math.max(dist , (i-j));
-                j--;
-            }else{
-                i--;
+        Stack<Integer> stack = new Stack<>();
+        // First, find the indices of elements in non-decreasing order.
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty() || nums[stack.peek()] > nums[i]) {
+                stack.push(i);
             }
         }
-
-        return dist;
+        
+        // Now, traverse the array from right to left and find the maximum width.
+        int maxWidth = 0;
+        for (int j = n - 1; j >= 0; j--) {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[j]) {
+                maxWidth = Math.max(maxWidth, j - stack.pop());
+            }
+        }
+        
+        return maxWidth;
     }
 }
