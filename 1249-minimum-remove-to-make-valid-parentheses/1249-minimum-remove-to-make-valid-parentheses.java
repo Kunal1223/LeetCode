37248bfba7d7e9cdea73690b6f7c199345c67953
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-07 01:04:03 +0530

    Time: 7 ms (88.23%), Space: 45.1 MB (87.87%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,59 +1,38 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-
-        int n = s.length();
-        if(n == 1) return s;
-        int count = 0;
-        String ans = "";
-
-        for(int i = 0 ; i < n ; i++){
-            char ch = s.charAt(i);
-
-            if(ch == '('){
-                count++;
-                ans += ch;
-            }
-            else if(ch == ')'){
-                count--;
-
-                if(count < 0){
-                    count = 0;
-                }else{
-                    ans += ch;
+        char[] chars = s.toCharArray();
+        int openCount = 0;
+        
+        // Forward pass: mark invalid closing parentheses and count open parentheses
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == '(') {
+                openCount++;
+            } else if (c == ')') {
+                if (openCount == 0) {
+                    chars[i] = '*'; // Mark invalid closing parenthesis
+                } else {
+                    openCount--;
                 }
-            }
-            else{
-                ans += ch;
-            }
-        }
-
-        n = ans.length();
-        count = 0;
-        String finala = "";
-
-        for(int i = n-1 ; i >= 0 ; i--){
-
-            char ch = ans.charAt(i);
-
-            if(ch == '('){
-                count--;
-
-                if(count < 0){
-                    count = 0;
-                }else{
-                    finala = ch + finala;
-                }
-            }
-
-            else if(ch == ')'){
-                count++;
-                finala = ch + finala;
-            }
-            else{
-                finala = ch + finala;
             }
         }
         
-        return finala;
+        // Backward pass: mark extra open parentheses
+        for (int i = chars.length - 1; i >= 0 && openCount > 0; i--) {
+            if (chars[i] == '(') {
+                chars[i] = '*'; // Mark extra open parenthesis
+                openCount--;
+            }
+        }
+        
+        // Construct result string
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            if (c != '*') {
+                result.append(c);
+            }
+        }
+        
+        return result.toString();
     }
 }
