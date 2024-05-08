0eec97652ee1c693b692841defda6980272bcbee
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-08 16:55:12 +0530

    Time: 43 ms (7.34%), Space: 48 MB (5.46%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,26 +1,43 @@
+// class Solution {
+//     public int firstUniqChar(String s) {
+        
+//         Map<Character, Integer> map = new HashMap<>();
+
+//         for (char c : s.toCharArray()) {
+//           map.put(c , map.getOrDefault(c , -1)+1);
+//         }
+        
+//         for(int i = 0 ; i < s.length() ; i++){
+            
+//             if(map.get(s.charAt(i)) == 0)
+//                 return i;
+        
+//         }
+        
+//         return -1;
+        
+//     }
+// }
+
+
+//################## Solve usign Queue ############################################
 class Solution {
     public int firstUniqChar(String s) {
-        
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character , Integer> map = new HashMap<>();
+        Queue<Integer> qu = new LinkedList<>();
 
-        for (char c : s.toCharArray()) {
-          if (map.containsKey(c)) {
-             map.put(c, map.get(c) + 1);
-          } else {
-             map.put(c, 0);
+        for(int i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            map.put(c , map.getOrDefault(c , 0)+1);
+            qu.add(i);
+        }
+
+        while(!qu.isEmpty()){
+            int ans = qu.peek();
+            if(map.get(s.charAt(qu.poll())) == 1){
+                return ans;
             }
         }
-
-        
-        
-        for(int i = 0 ; i < s.length() ; i++){
-            
-            if(map.get(s.charAt(i)) == 0)
-                return i;
-        
-        }
-        
         return -1;
-        
     }
 }
