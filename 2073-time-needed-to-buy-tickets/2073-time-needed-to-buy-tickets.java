1de65a609c7553c99d86e88dc680be570deaeff4
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-08 18:45:17 +0530

    Time: 2 ms (45.45%), Space: 40.7 MB (95.67%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,24 +1,48 @@
+// class Solution {
+//     public int timeRequiredToBuy(int[] tickets, int k) {
+
+//         int count = 0;
+//         Queue<Integer> qu = new LinkedList<>();
+
+//         for (int i = 0; i < tickets.length; i++) {
+//             qu.add(i);
+//         }
+
+//         while (tickets[k] != 0) {
+//             int temp = qu.peek();
+
+//             if (tickets[temp] != 0) {
+//                 tickets[temp]--;
+//                 count++;
+//             }
+            
+//             qu.poll();
+//             qu.add(temp);
+//         }
+//         return count;
+//     }
+// }
+
+//Brute Force
+
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
 
-        int count = 0;
-        Queue<Integer> qu = new LinkedList<>();
+        int n = tickets.length;
+        int count = 0 , ans = 0;
 
-        for (int i = 0; i < tickets.length; i++) {
-            qu.add(i);
-        }
-
-        while (tickets[k] != 0) {
-            int temp = qu.peek();
-
-            if (tickets[temp] != 0) {
-                tickets[temp]--;
-                count++;
-            }
+        while(tickets[k] != 0){
             
-            qu.poll();
-            qu.add(temp);
+            int ind = count % n;
+
+            if(tickets[ind] != 0){
+                tickets[ind]--;
+                ans++;
+            }
+
+            count++;
         }
-        return count;
+
+        return ans;
     }
 }
