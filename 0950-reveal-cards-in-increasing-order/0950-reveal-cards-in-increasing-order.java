676e3585bb8488bd2c2a507c6da6ce203fe75acf
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-09 23:35:43 +0530

    Time: 4 ms (90.76%), Space: 43.5 MB (58.37%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -1,31 +1,55 @@
-public class Solution {
-    public int[] deckRevealedIncreasing(int[] deck) {
-        int N = deck.length;
-        int[] result = new int[N];
+// public class Solution {
+//     public int[] deckRevealedIncreasing(int[] deck) {
+//         int N = deck.length;
+//         int[] result = new int[N];
 
+//         Arrays.sort(deck);
+
+//         return everyOther(deck, result, 0, 0, false);
+//     }
+
+//     private int[] everyOther(int[] deck, int[] result, int indexInDeck, int indexInResult, boolean skip) {
+//         int N = deck.length;
+
+//         if (indexInDeck == N) {
+//             return result;
+//         }
+
+//         while (indexInResult < N) {
+//             if (result[indexInResult] == 0) {
+//                 if (!skip) {
+//                     result[indexInResult] = deck[indexInDeck];
+//                     indexInDeck++;
+//                 }
+//                 skip = !skip;
+//             }
+//             indexInResult++;
+//         }
+
+//         return everyOther(deck, result, indexInDeck, 0, skip);
+//     }
+// }
+
+
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
         Arrays.sort(deck);
 
-        return everyOther(deck, result, 0, 0, false);
-    }
-
-    private int[] everyOther(int[] deck, int[] result, int indexInDeck, int indexInResult, boolean skip) {
-        int N = deck.length;
-
-        if (indexInDeck == N) {
-            return result;
+        Deque<Integer> ind = new ArrayDeque<>();
+        for(int i=0;i<deck.length;i++){
+            ind.addLast(i);
         }
 
-        while (indexInResult < N) {
-            if (result[indexInResult] == 0) {
-                if (!skip) {
-                    result[indexInResult] = deck[indexInDeck];
-                    indexInDeck++;
-                }
-                skip = !skip;
+        int[] ans = new int[deck.length];
+        int i=0;
+        
+        while(!ind.isEmpty()){
+            ans[ind.pollFirst()] = deck[i];
+            if(!ind.isEmpty()){
+                ind.addLast(ind.pollFirst());
             }
-            indexInResult++;
+            i++;
         }
-
-        return everyOther(deck, result, indexInDeck, 0, skip);
+        return ans;
     }
 }
