415fc4fddcf07c7ceba52daf2f6b87552ae304c0
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-13 16:53:14 +0530

    Time: 1 ms (81.95%), Space: 40.7 MB (66.00%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,25 +1,46 @@
 //Problems of the day
 
 
-class Solution {
-    public int reverse(int x) {
-        int result = 0;
+// class Solution {
+//     public int reverse(int x) {
+//         int result = 0;
         
-        while (x != 0) {
-            int lastDigit = x % 10;
+//         while (x != 0) {
+//             int lastDigit = x % 10;
             
            
-            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && lastDigit > 7)) {
-                return 0;
-            }
-            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && lastDigit < -8)) {
-                return 0;
-            }
+//             if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && lastDigit > 7)) {
+//                 return 0;
+//             }
+//             if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && lastDigit < -8)) {
+//                 return 0;
+//             }
             
-            result = result * 10 + lastDigit;
-            x /= 10;
+//             result = result * 10 + lastDigit;
+//             x /= 10;
+//         }
+
+//         return result;
+//     }
+// }
+
+class Solution {
+    public int reverse(int x) {
+
+        long ans = 0;
+        while (x != 0) {
+            int temp = x % 10;
+            ans = ans * 10 + temp;
+            x = x / 10;
+
+            if (ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE ) {
+                return 0;
+            }
         }
 
-        return result;
+        return (int)ans;
+
     }
 }
+
+
