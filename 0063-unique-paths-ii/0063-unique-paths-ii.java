08de7f156ce1aceec9cdb4fb8803103b7f5b3038
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-18 21:04:02 +0530

    Time: 0 ms (100.00%), Space: 42 MB (10.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,27 +1,29 @@
-// Problems of the days
-
 class Solution {
-
-    static int helper2(int arr[][]){
-        int m = arr.length;
-        int n = arr[0].length;
-
-        int dp[][] = new int[m+1][n+1];
-        for(var a : dp) Arrays.fill(a,0);
-        dp[m-1][n-1] = 1;
     
-        for(int i=m-1; i>=0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (i == m - 1 && j == n - 1) continue;
-                if (arr[i][j] == 1) dp[i][j] = 0;
-                else dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-            }
-        }
-        return dp[0][0];
+     int SolveByRecursion(int m , int n , int dp[][] , int[][] obstacleGrid){
+        if(m == 0 && n == 0) return 1;
+        if(m < 0 || n < 0) return 0;
+        if(obstacleGrid[m][n] == 1) return 0;
+        
+        
+        if(dp[m][n] != -1) return dp[m][n];
+        
+        int left = SolveByRecursion(m , n-1 , dp , obstacleGrid) ;
+        int up = SolveByRecursion(m-1 , n , dp , obstacleGrid);
+        
+        return dp[m][n] = (up + left);
     }
-    public int uniquePathsWithObstacles(int[][] arr) {
-
-       if(arr[arr.length-1][arr[0].length-1]==1) return 0;
-       return helper2(arr);
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int dp[][] = new int[m+1][n+1];
+        if(obstacleGrid[0][0] == 1) return 0;
+        
+        for(int a[] : dp){
+            Arrays.fill(a , -1);
+        }
+        
+        return SolveByRecursion(m-1 , n-1 , dp , obstacleGrid);
     }
 }
