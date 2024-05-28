70767103b857f53ab1540fe7c1768cae5c1e4a65
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-28 22:29:27 +0530

    Time: 2 ms (84.83%), Space: 46 MB (56.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,46 @@
+// class Solution {
+//      public void moveZeroes(int[] nums) {
+         
+//         int snowBallSize = 0; 
+         
+//         for (int i=0;i<nums.length;i++){
+            
+// 	        if (nums[i]==0){
+//                 snowBallSize++; 
+//             }
+            
+//             else if (snowBallSize > 0) {
+// 	            int t = nums[i];
+// 	            nums[i]=0;
+// 	            nums[i-snowBallSize]=t;
+//             }
+//         }
+//     }
+// }
+
+
 class Solution {
-     public void moveZeroes(int[] nums) {
-         
-        int snowBallSize = 0; 
-         
-        for (int i=0;i<nums.length;i++){
-            
-	        if (nums[i]==0){
-                snowBallSize++; 
-            }
-            
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t;
+    public void moveZeroes(int[] nums) {
+
+        int count = 0;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] == 0){
+                count++;
             }
         }
+
+        int k = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] != 0){
+                nums[k++] = nums[i];
+            }
+        }
+
+        while(count > 0){
+            nums[k++] = 0;
+            count--;
+        }
+        
     }
 }
