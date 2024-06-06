64b588ef42dd351ba2b7aaa3000349b3b9d48680
Kunal Kishor <kunalbth2002@gmail.com>
2024-06-06 17:29:57 +0530

    Time: 63 ms (17.08%), Space: 45.7 MB (34.63%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -1,35 +1,30 @@
 class Solution {
-    public boolean findsucessors(int[] hand, int groupSize, int i, int n) {
-        int f = hand[i] + 1;
-        hand[i] = -1;
-        int count = 1;
-        i += 1;
-        while (i < n && count < groupSize) {
-            if (hand[i] == f) {
-                f = hand[i] + 1;
-                hand[i] = -1;
-                count++;
-            }
-            i++;
-        }
-        if (count != groupSize)
-            return false;
-        else
-            return true;
-    }
-
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        int n = hand.length;
-        if (n % groupSize != 0)
+        if (hand.length % groupSize != 0) {
             return false;
-        Arrays.sort(hand);
-        int i = 0;
-        for (; i < n; i++) {
-            if (hand[i] >= 0) {
-                if (!findsucessors(hand, groupSize, i, n))
-                    return false;
+        }
+
+        // Create a frequency map
+        Map<Integer, Integer> countMap = new TreeMap<>();
+        for (int card : hand) {
+            countMap.put(card, countMap.getOrDefault(card, 0) + 1);
+        }
+
+        // Iterate through the keys in the TreeMap (sorted order)
+        for (int card : countMap.keySet()) {
+            int count = countMap.get(card);
+            if (count > 0) {
+                // We need to form `count` number of groups starting with `card`
+                for (int i = 1; i < groupSize; i++) {
+                    int nextCard = card + i;
+                    if (countMap.getOrDefault(nextCard, 0) < count) {
+                        return false;
+                    }
+                    countMap.put(nextCard, countMap.get(nextCard) - count);
+                }
             }
         }
+
         return true;
     }
 }
