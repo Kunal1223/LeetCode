a3cf43b9ac51b08bb2927c1518f71427f8bbef47
Kunal Kishor <kunalbth2002@gmail.com>
2024-06-22 11:50:00 +0530

    Time: 18 ms (29.21%), Space: 43.7 MB (99.55%) - LeetHub

--- 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.java
+++ 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.java
@@ -1,33 +1,38 @@
 class Solution {
     public boolean isValid(String s) {
-
-        int acount = 0 , bcount = 0 , ccount =0 ;
+        Stack<Character> st = new Stack<>();
         int n = s.length();
 
-        if(s.equals("aababcbccabc")) return false;
-
-        if( n < 3) return false;
+        if(n < 3) return false;
 
         for(int i = 0 ; i < n ; i++){
             char ch = s.charAt(i);
 
-            if(i >= 1 && ch == 'b'){
-                if(s.charAt(i-1) == 'b') return false;
+            if(ch == 'a') st.push(ch);
+
+            else if(ch == 'b'){
+                if(!st.isEmpty() && st.peek() == 'a'){
+                    st.push(ch);
+                }
+                else return false;
             }
+            else{
+                if(!st.isEmpty() && st.peek() == 'b'){
+                    st.pop();
 
-            if(i >= 1 && ch == 'c'){
-                if(s.charAt(i-1) == 'a') return false;
+                    if(!st.isEmpty() && st.peek() == 'a'){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
             }
-
-            if(ch == 'a') acount++;
-            else if(ch == 'b') bcount++;
-            else ccount++;
-
-            if(acount < bcount || acount < ccount || bcount < ccount) return false;
         }
 
-        if(acount == bcount && bcount == ccount) return true;
-        return false;
-        
+        if(!st.isEmpty()) return false;
+        else return true;
     }
 }
