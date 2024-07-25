9231a3f3b453c28f8a71b7889ac298727cb0df36
Kunal Kishor <kunalbth2002@gmail.com>
2024-07-25 10:56:39 +0530

    Time: 28 ms (50.43%), Space: 56.1 MB (53.26%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,19 +1,61 @@
 class Solution {
+
+    public void Merge(int nums[] , int st , int mid , int ed){
+        int n = mid - st + 1;
+        int m = ed - mid;
+
+        int L[] = new int[n];
+        int R[] = new int[m];
+
+        for(int i = 0 ; i < n ; i++){
+            L[i] = nums[st + i];
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            R[i] = nums[mid+1+i];
+        }
+
+        int i = 0 , j = 0;
+        int k = st;
+
+        while(i < n && j < m){
+            if(L[i] <= R[j]){
+                nums[k] = L[i];
+                i++;
+            }
+            else{
+                nums[k] = R[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while(i < n){
+            nums[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while(j < m){
+            nums[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public void mergeSort(int nums[] , int st , int ed){
+        if(st < ed){
+            int mid = st + (ed - st)/2;
+            mergeSort(nums , st , mid);
+            mergeSort(nums , mid + 1 , ed);
+            Merge(nums , st , mid , ed);
+        }
+    }
+
     public int[] sortArray(int[] nums) {
         int n = nums.length;
-
-        // for(int i = 0 ; i < n ; i++){
-        //     for(int j = i+1 ; j < n ; j++){
-        //         if(nums[i] > nums[j]){
-        //             int temp = nums[i];
-        //             nums[i] = nums[j];
-        //             nums[j] = temp;
-        //         }
-        //     }
-        // }
-
-        Arrays.sort(nums);
-
+        mergeSort(nums , 0 , n-1);
         return nums;
     }
 }
