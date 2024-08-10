40ede08ec4abf02b2c57096200e3bbb2517ef2ac
Kunal Kishor <kunalbth2002@gmail.com>
2024-08-10 16:08:58 +0530

    Time: 0 ms (100%), Space: 40 MB (76.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,52 +1,23 @@
-// ############### Solve By Using HashMap ######################################
-
-// class Solution {
-    
-//     int solve(Map<Integer , Integer> mp , int n){
-
-//         if(n == 0 || n == 1){
-//             return 1;
-//         }
-        
-//         if(!mp.containsKey(n)){
-//             mp.put(n , solve(mp , n-1) + solve(mp,n-2));
-//         }
-        
-//         return mp.get(n);
-    
-//     }
-    
-    
-//     public int climbStairs(int n) {
-        
-//         Map<Integer , Integer> mp = new HashMap<>();
-        
-//         return solve(mp , n);
-        
-//     }
-// }
-
-
-// ################  Solve By Using Dynamic Programing #################
-
 class Solution {
 
-    public int SolveByDynamic(int dp[] , int n){
-        if(n == 1 || n == 0) return 1;
+    public int waytoclimb(int n , int []dp){
+        if(n == 0) return 1;
+        if(n < 0) return 0;
 
-        if(dp[n] != -1)
-            return dp[n];
+        if(dp[n] != -1) return dp[n];
 
-        return dp[n] = SolveByDynamic(dp , n-1) + SolveByDynamic(dp , n-2);
+        dp[n] = waytoclimb(n-1 , dp) + waytoclimb(n-2 , dp);
+        return dp[n];
     }
 
-    public int climbStairs(int n) {
-        if(n == 1 || n == 0 ) return 1;
 
-        int dp[] = new int [n+1];
+    public int climbStairs(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        int dp[] = new int[n+1];
         Arrays.fill(dp , -1);
 
-        return SolveByDynamic(dp , n);
-        
+        return waytoclimb(n , dp);
     }
 }
