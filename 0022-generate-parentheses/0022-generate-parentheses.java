eb3fd7f2479f2f165490e98fc9967dd35cd83f9d
Kunal Kishor <kunalbth2002@gmail.com>
2024-08-11 15:05:24 +0530

    Time: 1 ms (86.13%), Space: 43.2 MB (49.55%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,23 +1,22 @@
 class Solution {
+
+    public void generate(List<String> list , int n , int left , int right , String s){
+        if(s.length() == 2*n){
+            list.add(s);
+        }
+
+        if(left < n){
+            generate(list , n , left+1 , right , s+"(");
+        }
+
+        if(right < left){
+            generate(list , n , left , right+1 , s+")");
+        }
+    }
+
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<String>();
-        recurse(res, 0, 0, "", n);
-        return res;
+        List<String> list = new ArrayList<>();
+        generate(list , n , 0 , 0 , "");
+        return list;
     }
-    
-    public void recurse(List<String> res, int left, int right, String s, int n) {
-        if (s.length() == n * 2) {
-            res.add(s);
-            return;
-        }
-        
-        if (left < n) {
-            recurse(res, left + 1, right, s + "(", n);
-        }
-        
-        if (right < left) {
-            recurse(res, left, right + 1, s + ")", n);
-        }
-    }
-	// See above tree diagram with parameters (left, right, s) for better understanding
 }
